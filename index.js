0519b0fa43b6e0bc65c2b36a2b8030dac0230446
Brandon <brandon.miller@pendo.io>
2021-10-01 13:27:27 -0400

    Array test

---
+++ index.js
@@ -0,0 +1,4 @@
+if (document.getElementById('vehicle1').checked) {
+    vehicle1.classList.add("checked")
+} else {
+    verhicle1.classList.remove("checked") && vehicle1.classList.add("unchecked")};
