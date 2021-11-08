6b24a60c3c4e691cab14aec88ac9dbe94aa6ad93
Brandon <brandon.miller@pendo.io>
2021-11-08 09:33:32 -0500

    customer metadata

--- index.js
+++
@@ -1,4 +0,0 @@
-if (document.getElementById('vehicle1').checked) {
-    vehicle1.classList.add("checked")
-} else {
-    verhicle1.classList.remove("checked") && vehicle1.classList.add("unchecked")};
