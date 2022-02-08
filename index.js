5c74f2319b8ee7191cd2ca7f8ce7df59ed1f0be6
Brandon <brandon.miller@pendo.io>
2022-02-08 15:58:35 -0500

    making sure accountid is removed

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
 console.log("JS is linked")
 
+var badge = document.getElementById('badgeIdHere');
+var i=5;
+
+while (i>0){
+    if(badge.addEventListener("click",()=>{
+        i--;
+    }));       
+}
+
+if(i==0){
+    badge.classList.add('hidden') //display:none would be used in the CSS perhaps
+} 
+
+if (pendo.showGuideById('guideIdHere')=='true'){
+    badge.classList.add('hidden')
+}
